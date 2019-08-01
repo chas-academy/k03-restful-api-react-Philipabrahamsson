309999c3824ed1e8023c5b04e9b7da80eb42e849
Philip Abrahamsson <Philip@Philis-Air.local>
2019-08-01 16:01:27 +0200

    Restructured the code a little bit and made a new folder for models

--- routes/customers.js
+++ routes/customers.js
@@ -3,22 +3,6 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  },
-    phone: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50
-    }
-}));
-
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
   res.send(customers);
@@ -28,9 +12,9 @@ router.post('/', async (req, res) => {
   const { error } = validateCustomer(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  let customer = new Customer({
+  let customer = new Customer({ 
     name: req.body.name,
-    phone: req.body.phone 
+    phone: req.body.phone
   });
   customer = await customer.save();
   
@@ -40,9 +24,12 @@ router.put('/:id', async (req, res) => {
   const { error } = validateCustomer(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  const customer = await Customer.findByIdAndUpdate(req.params.id, { name: req.body.name }, { 
-    new: true});
-     
+  const customer = await Customer.findByIdAndUpdate(req.params.id,
+    { 
+      name: req.body.name,
+      phone: req.body.phone
+    }, { new: true });
+
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
   
   res.send(customer);
@@ -63,14 +50,5 @@ router.get('/:id', async (req, res) => {
   
   res.send(customer);
 });
-
-function validateCustomer(customer) {
-  const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    phone: Joi.string().min(5).max(50).required()
-  };
-
-  return Joi.validate(customer, schema);
-}
 
 module.exports = router;
