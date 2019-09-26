77036667a266bd54cf4eeb12865ec6714513d271
Philip Abrahamsson <Philip@Philis-Air.local>
2019-09-26 19:59:43 +0200

    made some inline styles and restructured some code

--- src/components/HandleSearch.js
+++ src/components/HandleSearch.js
@@ -2,34 +2,43 @@ import React, { useEffect, useState } from 'react';
 import * as constants from '../constants/Constants';
 import { withRouter } from 'react-router-dom'
 
-const HandleSearch = (props) => {
-  
+const HandleSearch = ({ updateMovies }) => {
+
   const [movieQuery, updateMovieQuery] = useState('');
 
-  const search = (event) => {
-    event.preventDefault();
-    const baseUrl = `https://api.themoviedb.org/3`;
-    const query = `/search/movie?api_key=978764f7929f89a0f0d4c0cfd6b531d4&query=
-    ${movieQuery.split(" ").join("+")}`;
-    fetch(baseUrl + query).then(data => data.json()).then(data => console.log(data))
-      .catch(err => console.error(err))
-    props.history.push('/store')
-  }
-  return (
-    <div>
-      <h2>Movie Store</h2>
-      <form onSubmit={(event) => search(event)}>
+  const handleMovieQueryInput = event => updateMovieQuery(event.target.value)
 
+  const search = () => {
+    const query = `/search/movie?api_key=978764f7929f89a0f0d4c0cfd6b531d4&query=
+    ${encodeURIComponent(movieQuery)}`;
+    fetch(constants.baseUrl + query)
+      .then(data => data.json())
+      .then(data => updateMovies(data.results))
+      .catch(err => console.error(err))
+  }
+
+  return (
+    <div style={{ width: '95%', display: 'flex', justifyContent: 'center' }}>
+      <div style={{ width: '85%', flexGrow: '1' }}>
         <input
-          onChange={(event) => {
-            updateMovieQuery(event.target.value)
-          }}
-          placeholder='Search for a movie' />
-        <button type='submit'>Search</button>
-        </form>
+          onChange={handleMovieQueryInput}
+          value={movieQuery}
+          placeholder='Search for a movie'
+          style={
+            {
+              border: 'none',
+              width: '90%',
+              borderBottom: '2px solid #000000',
+              fontSize: '18px',
+              padding: '8px',
+              outline: 'none'
+            }
+          } />
+        </div>
+      <button onClick={search} style={{ width: '10%', background: 'none', border: '2px solid #000000', fontSize: '16px', padding: '8px', borderRadius: '6px', }}>Search</button>
     </div>
-      )
-    }
-    
-    
+  )
+}
+
+
 export default withRouter(HandleSearch);
